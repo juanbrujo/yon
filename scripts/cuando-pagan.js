9e114075234ca05e9ba2dffdfe9296532cb53cd6
Jorge Epuñan <juanbrujo@gmail.com>
2016-06-21 14:20:32 -0400

    nuevo cuando pagan

---
+++ scripts/cuando-pagan.js
@@ -0,0 +1,35 @@
+// Description:
+//   TODO
+//
+// Dependencies:
+//   moment-business-days
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   hubot cuando pagan
+//
+// Author:
+//   @hectorpalmatellez
+
+var moment = require('moment-business-days');
+
+module.exports = function gardel(robot) {
+  'use strict';
+
+  var today = moment().format('D');
+  var lastBusinessDay = moment().endOf('month').isBusinessDay() ? moment().endOf('month').format('D') : moment().endOf('month').prevBusinessDay().format('D');
+  var nameLastDay = moment().locale('es').endOf('month').isBusinessDay() ? moment().locale('es').endOf('month').format('dddd') : moment().locale('es').endOf('month').prevBusinessDay().format('dddd');
+  var dayCount = lastBusinessDay - today;
+
+  robot.respond(/cu[áa]ndo pagan/, function(msg) {
+    var message = '';
+    if (dayCount === 0) {
+      message = `:tada: Hoy pagan :tada:`;
+    } else {
+      message = `Faltan ${dayCount} días para que paguen. Este mes pagan el ${lastBusinessDay}, que cae ${nameLastDay} :tired_face:`;
+    }
+    return msg.send(message);
+  });
+};
