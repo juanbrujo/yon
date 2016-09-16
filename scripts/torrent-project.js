9b823e5350f10f078d785515de6590fb5615e631
Jorge Epuñan <jepunan@veca-100319.ml.com>
2016-09-16 12:12:04 -0300

    fixed, cloud flare bypass
    :fap:

--- scripts/torrent-project.js
+++ scripts/torrent-project.js
@@ -1,8 +1,9 @@
 // Description:
-//   Get a torrent link from torrentproject.se
+//   Search torrents from torrentproject.se
 //
 //  Dependencies:
 //    cheerio
+//    cloudscraper
 //
 // Commands:
 //   hubot torrent <query>
@@ -11,6 +12,7 @@
 //   @jorgeepunan
 
 var cheerio = require('cheerio');
+var cloudscraper = require('cloudscraper');
 
 module.exports = function(robot) {
   robot.respond(/torrent (.*)/i, function(msg) {
@@ -20,34 +22,38 @@ module.exports = function(robot) {
     var busqueda = msg.match[1];
     var url = 'https://torrentproject.se/?t=' + busqueda.split(' ').join('+');
 
-    msg.robot.http(url).get()(function(err, res, body) {
-
-      var $ = cheerio.load(body);
-      var resultados = [];
-
-      $('#similarfiles > div:not(.gac_bb)').each(function() {
-        var title = $(this).find('a').text();
-        var link = $(this).find('a').attr('href');
-
-        resultados.push( title + ' | ' + link );
-      });
-
-      if(resultados.length > 0) {
-        var limiteResultados = (resultados.length > 4) ? 3 : resultados.length;
-        var plural = resultados.length > 1 ? ['n','s'] : ['',''];
-        msg.send('Se ha'+plural[0]+' encontrado '+ resultados.length + ' resultado'+plural[1]);
-        for (var i=0; i < limiteResultados; i++) {
-          var conteo = i + 1;
-          msg.send(conteo + ': ' + resultados[i]);
-        }
-        if(resultados.length > limiteResultados) {
-          msg.send('Otros resultados en: '+ url);
-        }
+    cloudscraper.get(url, function(error, response, body) {
+      if (error) {
+        console.log('Ocurrió un error :(');
       } else {
-        msg.send('No se han encontrado resultados sobre '+ busqueda);
-      }
+        var $ = cheerio.load(body);
+        var resultados = [];
 
+        $('#similarfiles > div:not(.gac_bb)').each(function() {
+          var title = $(this).find('a').text();
+          var link = $(this).find('a').attr('href');
+
+          resultados.push( title + ' | ' + link );
+        });
+
+        if(resultados.length > 0) {
+          var limiteResultados = (resultados.length > 4) ? 3 : resultados.length;
+          var plural = resultados.length > 1 ? ['n','s'] : ['',''];
+          msg.send('Se ha'+plural[0]+' encontrado '+ resultados.length + ' resultado'+plural[1]);
+          for (var i=0; i < limiteResultados; i++) {
+            var conteo = i + 1;
+            msg.send(conteo + ': ' + resultados[i]);
+          }
+          if(resultados.length > limiteResultados) {
+            msg.send('Otros resultados en: '+ url);
+          }
+        } else {
+          msg.send('No se han encontrado resultados sobre '+ busqueda);
+        }
+
+      }
     });
 
   });
+
 };
